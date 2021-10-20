9bc317b344e8755ddb9e89b64fce6d13b169e3ca
Julianne Pereira Santos Pinheiro <jupsantos@outlook.com.br>
2021-10-20 20:28:42 -0300

    Melhorias no codigo

--- JavaInvest/src/br/com/generation/javainvest/JavaInvest.java
+++ JavaInvest/src/br/com/generation/javainvest/JavaInvest.java
@@ -10,7 +10,7 @@ public class JavaInvest {
 		Usuario user = new Usuario();
 		
 		int opcao;
-		double despesaRenda, resto, rendaTotal, despesa, despesaTotal, lazer, investimento;
+		double despesaRenda, rendaTotal, despesaTotal;
 		
 		
 		
@@ -45,200 +45,13 @@ public class JavaInvest {
 					+ "\n4 - Ajuda(?)", "Java Invest", JOptionPane.INFORMATION_MESSAGE));
 			switch(opcao) {
 				case 1: 
-					if(despesaRenda >= 0 && despesaRenda < 50 ) {
-						JOptionPane.showMessageDialog(null,"As suas despesas representam: " + Math.round(despesaRenda)  + "% da sua renda", "Java Invest", JOptionPane.INFORMATION_MESSAGE);
-						resto = despesaRenda - 40;
-						if(resto >= 0) {
-							despesa = (despesaRenda/100) * rendaTotal;
-							lazer = calc.calcularValorRestante(rendaTotal, (50 - resto));
-							investimento = calc.calcularValorRestante(rendaTotal, 10);
-							JOptionPane.showMessageDialog(null, user.getNome() + ", com base nos seus ganhos, despesas e perfil de investimento baixo, \nas indicações para gerenciamento financeiro são:" 
-															+ "\n\nR$ " + Math.round(despesa) + ", para destinar as despesas fixas;" + "\nR$ " 
-															+ Math.round(lazer) + ", para lazer; \nR$ " + Math.round(investimento) + " para investir.", "Java Invest", JOptionPane.INFORMATION_MESSAGE);
-						}
-						else {
-							despesa = (despesaRenda/100) * rendaTotal;
-							lazer = calc.calcularValorRestante(rendaTotal, 50);
-							investimento = calc.calcularValorRestante(rendaTotal, (10 - resto));
-							JOptionPane.showMessageDialog(null, user.getNome() + ", com base nos seus ganhos, despesas e perfil de investimento baixo, \nas indicações para gerenciamento financeiro são:"  
-															+ "\n\nR$ " + Math.round(despesa) + ", para destinar as despesas fixas;" + "\nR$ " 
-															+ Math.round(lazer) + ", para lazer; \nR$ " + Math.round(investimento) + " para investir.", "Java Invest", JOptionPane.INFORMATION_MESSAGE);
-						}
-					}
-					else if(despesaRenda >= 50 && despesaRenda < 60) {
-						JOptionPane.showMessageDialog(null,"As suas despesas representam: " + Math.round(despesaRenda) + "% da sua renda");
-						resto = despesaRenda - 50;
-						if(resto >= 0) {
-							despesa = (despesaRenda/100) * rendaTotal;
-							lazer = calc.calcularValorRestante(rendaTotal, (40 - resto));
-							investimento = calc.calcularValorRestante(rendaTotal, 10);
-							JOptionPane.showMessageDialog(null, user.getNome() + ", com base nos seus ganhos, despesas e perfil de investimento baixo, \nas indicações para gerenciamento financeiro são:"  
-									+ "\n\nR$ " + Math.round(despesa) + ", para destinar as despesas fixas;" + "\nR$ " 
-									+ Math.round(lazer) + ", para lazer; \nR$ " + Math.round(investimento) + " para investir.", "Java Invest", JOptionPane.INFORMATION_MESSAGE);
-						}
-						else {
-							despesa = (despesaRenda/100) * rendaTotal;
-							lazer = calc.calcularValorRestante(rendaTotal, 40);
-							investimento = calc.calcularValorRestante(rendaTotal, (10 - resto));
-							JOptionPane.showMessageDialog(null, user.getNome() + ". com base nos seus ganhos, despesas e perfil de investimento baixo, \nas indicações para gerenciamento financeiro são:"  
-									+ "\n\nR$ " + Math.round(despesa) + ", para destinar as despesas fixas;" + "\nR$ " 
-									+ Math.round(lazer) + ", para lazer; \nR$ " + Math.round(investimento) + " para investir.", "Java Invest", JOptionPane.INFORMATION_MESSAGE);
-						}
-						
-					}
-					else if(despesaRenda >= 60 && despesaRenda <= 65) {
-						JOptionPane.showMessageDialog(null,"As suas despesas representam: " + Math.round(despesaRenda) + "% da sua renda");
-						resto = despesaRenda - 60;
-						if(resto >= 0) {
-							despesa = (despesaRenda/100) * rendaTotal;
-							lazer = calc.calcularValorRestante(rendaTotal, (37 - resto));
-							investimento = calc.calcularValorRestante(rendaTotal, 3);
-							JOptionPane.showMessageDialog(null, user.getNome() + ", com base nos seus ganhos, despesas e perfil de investimento baixo, \nas indicações para gerenciamento financeiro são:"  
-									+ "\n\nR$ " + Math.round(despesa) + ", para destinar as despesas fixas;" + "\nR$ " 
-									+ Math.round(lazer) + ", para lazer; \nR$ " + Math.round(investimento) + " para investir.", "Java Invest", JOptionPane.INFORMATION_MESSAGE);
-						}
-						else {
-							despesa = (despesaRenda/100) * rendaTotal;
-							lazer = calc.calcularValorRestante(rendaTotal, 37);
-							investimento = calc.calcularValorRestante(rendaTotal, (3 - resto));
-							JOptionPane.showMessageDialog(null, user.getNome() + ", com base nos seus ganhos, despesas e perfil de investimento baixo, \nas indicações para gerenciamento financeiro são:"  
-									+ "\n\nR$ " + Math.round(despesa) + ", para destinar as despesas fixas;" + "\nR$ " 
-									+ Math.round(lazer) + ", para lazer; \nR$ " + Math.round(investimento) + " para investir.", "Java Invest", JOptionPane.INFORMATION_MESSAGE);
-						}
-					}
-					else {
-						JOptionPane.showMessageDialog(null, "Oops! " + user.getNome() + ", parece que você ainda não tem perfil para investir!", "Java Invest", JOptionPane.INFORMATION_MESSAGE);
-					}
+					PerfilBaixo.perfil(user, calc, despesaRenda, rendaTotal);
 				break;
 				case 2: 
-					if(despesaRenda >= 0 && despesaRenda < 50 ) {
-						JOptionPane.showMessageDialog(null,"Porcentagem da despesa: " + Math.round(despesaRenda) + "%", "Java Invest", JOptionPane.INFORMATION_MESSAGE);
-						resto = despesaRenda - 40;	
-						if(resto >= 0) {
-							despesa = (despesaRenda/100) * rendaTotal;
-							lazer = calc.calcularValorRestante(rendaTotal, (45 - resto));
-							investimento = calc.calcularValorRestante(rendaTotal, 15);
-							JOptionPane.showMessageDialog(null, user.getNome() + ", com base nos seus ganhos, despesas e perfil de investimento intermediário, \nas indicações para gerenciamento financeiro são:" 
-															+ "\n\nR$ " + Math.round(despesa) + ", para destinar as despesas fixas;" + "\nR$ " 
-															+ Math.round(lazer) + ", para lazer; \nR$ " + Math.round(investimento) + " para investir.", "Java Invest", JOptionPane.INFORMATION_MESSAGE);
-						}
-						else {
-							despesa = (despesaRenda/100) * rendaTotal;
-							lazer = calc.calcularValorRestante(rendaTotal, 45);
-							investimento = calc.calcularValorRestante(rendaTotal, (15 - resto));
-							JOptionPane.showMessageDialog(null, user.getNome() + ", com base nos seus ganhos, despesas e perfil de investimento intermediário, \nas indicações para gerenciamento financeiro são:"  
-															+ "\n\nR$ " + Math.round(despesa) + ", para destinar as despesas fixas;" + "\nR$ " 
-															+ Math.round(lazer) + ", para lazer; \nR$ " + Math.round(investimento) + " para investir.", "Java Invest", JOptionPane.INFORMATION_MESSAGE);
-						}
-					}
-					else if(despesaRenda >= 50 && despesaRenda < 60) {
-						JOptionPane.showMessageDialog(null,"Porcentagem da despesa: " + Math.round(despesaRenda) + "%", "Java Invest", JOptionPane.INFORMATION_MESSAGE);
-						resto = despesaRenda - 50;	
-						if(resto >= 0) {
-							despesa = (despesaRenda/100) * rendaTotal;
-							lazer = calc.calcularValorRestante(rendaTotal, (30 - resto));
-							investimento = calc.calcularValorRestante(rendaTotal, 20);
-							JOptionPane.showMessageDialog(null, user.getNome() + ", com base nos seus ganhos, despesas e perfil de investimento intermediário, \nas indicações para gerenciamento financeiro são:" 
-															+ "\n\nR$ " + Math.round(despesa) + ", para destinar as despesas fixas;" + "\nR$ " 
-															+ Math.round(lazer) + ", para lazer; \nR$ " + Math.round(investimento) + " para investir.", "Java Invest", JOptionPane.INFORMATION_MESSAGE);
-						}
-						else {
-							despesa = (despesaRenda/100) * rendaTotal;
-							lazer = calc.calcularValorRestante(rendaTotal, 30);
-							investimento = calc.calcularValorRestante(rendaTotal, (20 - resto));
-							JOptionPane.showMessageDialog(null, user.getNome() + ", com base nos seus ganhos, despesas e perfil de investimento intermediário, \nas indicações para gerenciamento financeiro são:"  
-															+ "\n\nR$ " + Math.round(despesa) + ", para destinar as despesas fixas;" + "\nR$ " 
-															+ Math.round(lazer) + ", para lazer; \nR$ " + Math.round(investimento) + " para investir.", "Java Invest", JOptionPane.INFORMATION_MESSAGE);
-						}
-					}
-					else if(despesaRenda >= 60 && despesaRenda <= 65) {
-						JOptionPane.showMessageDialog(null,"Porcentagem da despesa: " + Math.round(despesaRenda) + "%", "Java Invest", JOptionPane.INFORMATION_MESSAGE);
-						resto = despesaRenda - 60;	
-						if(resto >= 0) {
-							despesa = (despesaRenda/100) * rendaTotal;
-							lazer = calc.calcularValorRestante(rendaTotal, (20 - resto));
-							investimento = calc.calcularValorRestante(rendaTotal, 20);
-							JOptionPane.showMessageDialog(null, user.getNome() + ", com base nos seus ganhos, despesas e perfil de investimento intermediário, \nas indicações para gerenciamento financeiro são:" 
-															+ "\n\nR$ " + Math.round(despesa) + ", para destinar as despesas fixas;" + "\nR$ " 
-															+ Math.round(lazer) + ", para lazer; \nR$ " + Math.round(investimento) + " para investir.", "Java Invest", JOptionPane.INFORMATION_MESSAGE);
-						}
-						else {
-							despesa = (despesaRenda/100) * rendaTotal;
-							lazer = calc.calcularValorRestante(rendaTotal, 20);
-							investimento = calc.calcularValorRestante(rendaTotal, (20 - resto));
-							JOptionPane.showMessageDialog(null, user.getNome() + ", com base nos seus ganhos, despesas e perfil de investimento intermediário, \nas indicações para gerenciamento financeiro são:"  
-															+ "\n\nR$ " + Math.round(despesa) + ", para destinar as despesas fixas;" + "\nR$ " 
-															+ Math.round(lazer) + ", para lazer; \nR$ " + Math.round(investimento) + " para investir.", "Java Invest", JOptionPane.INFORMATION_MESSAGE);
-						}
-					}
-					else {
-						JOptionPane.showMessageDialog(null, "Oops! " + user.getNome() + ", parece que você ainda não tem perfil para investir!", "Java Invest", JOptionPane.INFORMATION_MESSAGE);
-					}
+					PerfilIntermediario.perfil(user, calc, despesaRenda, rendaTotal);
 				break;
 				case 3:
-					if(despesaRenda >= 0 && despesaRenda < 50 ) {
-						JOptionPane.showMessageDialog(null,"Porcentagem da despesa: " + Math.round(despesaRenda) + "%", "Java Invest", JOptionPane.INFORMATION_MESSAGE);
-						resto = despesaRenda - 40;	
-						if(resto >= 0) {
-							despesa = (despesaRenda/100) * rendaTotal;
-							lazer = calc.calcularValorRestante(rendaTotal, (35 - resto));
-							investimento = calc.calcularValorRestante(rendaTotal, 25);
-							JOptionPane.showMessageDialog(null, user.getNome() + ", com base nos seus ganhos, despesas e perfil de investimento alto, \nas indicações para gerenciamento financeiro são:" 
-															+ "\n\nR$ " + Math.round(despesa) + ", para destinar as despesas fixas;" + "\nR$ " 
-															+ Math.round(lazer) + ", para lazer; \nR$ " + Math.round(investimento) + " para investir.", "Java Invest", JOptionPane.INFORMATION_MESSAGE);
-						}
-						else {
-							despesa = (despesaRenda/100) * rendaTotal;
-							lazer = calc.calcularValorRestante(rendaTotal, 35);
-							investimento = calc.calcularValorRestante(rendaTotal, (25 - resto));
-							JOptionPane.showMessageDialog(null, user.getNome() + ", com base nos seus ganhos, despesas e perfil de investimento alto, \nas indicações para gerenciamento financeiro são:"  
-															+ "\n\nR$ " + Math.round(despesa) + ", para destinar as despesas fixas;" + "\nR$ " 
-															+ Math.round(lazer) + ", para lazer; \nR$ " + Math.round(investimento) + " para investir.", "Java Invest", JOptionPane.INFORMATION_MESSAGE);
-						}
-					}
-					else if(despesaRenda >= 50 && despesaRenda < 60) {
-						JOptionPane.showMessageDialog(null,"Porcentagem da despesa: " + Math.round(despesaRenda) + "%", "Java Invest", JOptionPane.INFORMATION_MESSAGE);
-						resto = despesaRenda - 50;	
-						if(resto >= 0) {
-							despesa = (despesaRenda/100) * rendaTotal;
-							lazer = calc.calcularValorRestante(rendaTotal, (20 - resto));
-							investimento = calc.calcularValorRestante(rendaTotal, 30);
-							JOptionPane.showMessageDialog(null, user.getNome() + ", com base nos seus ganhos, despesas e perfil de investimento alto, \nas indicações para gerenciamento financeiro são:" 
-															+ "\n\nR$ " + Math.round(despesa) + ", para destinar as despesas fixas;" + "\nR$ " 
-															+ Math.round(lazer) + ", para lazer; \nR$ " + Math.round(investimento) + " para investir.", "Java Invest", JOptionPane.INFORMATION_MESSAGE);
-						}
-						else {
-							despesa = (despesaRenda/100) * rendaTotal;
-							lazer = calc.calcularValorRestante(rendaTotal, 20);
-							investimento = calc.calcularValorRestante(rendaTotal, (30 - resto));
-							JOptionPane.showMessageDialog(null, user.getNome() + ", com base nos seus ganhos, despesas e perfil de investimento alto, \nas indicações para gerenciamento financeiro são:"  
-															+ "\n\nR$ " + Math.round(despesa) + ", para destinar as despesas fixas;" + "\nR$ " 
-															+ Math.round(lazer) + ", para lazer; \nR$ " + Math.round(investimento) + " para investir.", "Java Invest", JOptionPane.INFORMATION_MESSAGE);
-						}
-					}
-					else if(despesaRenda >= 60 && despesaRenda <= 65) {
-						JOptionPane.showMessageDialog(null,"Porcentagem da despesa: " + Math.round(despesaRenda) + "%", "Java Invest", JOptionPane.INFORMATION_MESSAGE);
-						resto = despesaRenda - 60;	
-						if(resto >= 0) {
-							despesa = (despesaRenda/100) * rendaTotal;
-							lazer = calc.calcularValorRestante(rendaTotal, (10 - resto));
-							investimento = calc.calcularValorRestante(rendaTotal, 30);
-							JOptionPane.showMessageDialog(null, user.getNome() + ", com base nos seus ganhos, despesas e perfil de investimento alto, \nas indicações para gerenciamento financeiro são:" 
-															+ "\n\nR$ " + Math.round(despesa) + ", para destinar as despesas fixas;" + "\nR$ " 
-															+ Math.round(lazer) + ", para lazer; \nR$ " + Math.round(investimento) + " para investir.", "Java Invest", JOptionPane.INFORMATION_MESSAGE);
-						}
-						else {
-							despesa = (despesaRenda/100) * rendaTotal;
-							lazer = calc.calcularValorRestante(rendaTotal, 10);
-							investimento = calc.calcularValorRestante(rendaTotal, (30 - resto));
-							JOptionPane.showMessageDialog(null, user.getNome() + ", com base nos seus ganhos, despesas e perfil de investimento alto, \nas indicações para gerenciamento financeiro são:"  
-															+ "\n\nR$ " + Math.round(despesa) + ", para destinar as despesas fixas;" + "\nR$ " 
-															+ Math.round(lazer) + ", para lazer; \nR$ " + Math.round(investimento) + " para investir.", "Java Invest", JOptionPane.INFORMATION_MESSAGE);
-						}
-					}
-					else {
-						JOptionPane.showMessageDialog(null, "Oops! " + user.getNome() + ", parece que você ainda não tem perfil para investir!", "Java Invest", JOptionPane.INFORMATION_MESSAGE);
-					}
+					PerfilAlto.perfil(user, calc, despesaRenda, rendaTotal);
 				break;
 				case 4: 
 					
